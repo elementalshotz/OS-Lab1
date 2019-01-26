851de31155c4ee7a73bc6c18d3b25e5b719da4c7
Erik Kamph <erikkamph38@gmail.com>
2019-01-26 15:25:46 +0100

    hello
    Co-Authored-By: oskar berglund <berglundoskar97@hotmail.com>

--- wrapper.c
+++ wrapper.c
@@ -8,24 +8,28 @@ int MQcreate (mqd_t * mq, char * name)
     /* Creates a mailslot with the specified name. Uses mq as reference pointer, so that you can 	reach the handle from anywhere */
     /* Should be able to handle a messages of any size */
     /* Should return 1 on success and 0 on fail*/
-
+    return 0;
 }
+
 int MQconnect (mqd_t * mq, char * name)
 {
     /* Connects to an existing mailslot for writing Uses mq as reference pointer, so that you can 	reach the handle from anywhere*/
     /* Should return 1 on success and 0 on fail*/
+    return 0;
 }
 
 int MQread (mqd_t * mq, void ** refBuffer)
 {
     /* Read a msg from a mailslot, return nr Uses mq as reference pointer, so that you can 		reach the handle from anywhere */
     /* of successful bytes read              */
+    return 0;
 }
 
 int MQwrite (mqd_t * mq, void * sendBuffer)
 {
     /* Write a msg to a mailslot, return nr Uses mq as reference pointer, so that you can 	     reach the handle from anywhere*/
     /* of successful bytes written         */
+    return 0;
 }
 
 int MQclose(mqd_t * mq, char * name)
@@ -33,7 +37,9 @@ int MQclose(mqd_t * mq, char * name)
     /* close a mailslot, returning whatever the service call returns Uses mq as reference pointer, so that you can
     reach the handle from anywhere*/
     /* Should return 1 on success and 0 on fail*/
+    return 0;
 }
+
 int threadCreate(void * functionCall, int threadParam)
 {
 	/* Creates a thread running threadFunc */
